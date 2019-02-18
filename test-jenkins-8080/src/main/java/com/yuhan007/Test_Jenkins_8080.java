74dbfe9731660965f2dacda41ca2204d39a59ec9
yuhan007 <781567564@qq.com>
2019-02-18 18:11:04 +0800

    upload

---
+++ test-jenkins-8080/src/main/java/com/yuhan007/Test_Jenkins_8080.java
@@ -0,0 +1,12 @@
+package com.yuhan007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test_Jenkins_8080 {
+    public static void main(String[] args) {
+        SpringApplication.run(Test_Jenkins_8080.class, args);
+        System.out.println("a");
+    }
+}
---
+++ test-jenkins-8080/src/main/java/com/yuhan007/bean/Apple.java
@@ -0,0 +1,9 @@
+package com.yuhan007.bean;
+
+public class Apple {
+    private String name="张三1";
+
+    public String getName() {
+        return name;
+    }
+}
