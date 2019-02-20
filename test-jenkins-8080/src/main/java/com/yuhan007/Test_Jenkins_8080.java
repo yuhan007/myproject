a51a6a844d2059a037c6a6ef8bf880120569d1a7
yuhan007 <781567564@qq.com>
2019-02-20 11:52:16 +0800

    提交

--- test-jenkins-8080/src/main/java/com/yuhan007/Test_Jenkins_8080.java
+++ test-jenkins-8080/src/main/java/com/yuhan007/Test_Jenkins_8080.java
@@ -2,13 +2,20 @@ package com.yuhan007;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Test_Jenkins_8080 {
+public class Test_Jenkins_8080 extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Test_Jenkins_8080.class, args);
         System.out.println("a");
         System.out.println("a");
         System.out.println("a");
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
