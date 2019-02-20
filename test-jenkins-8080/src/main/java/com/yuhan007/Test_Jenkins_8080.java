c7329d1aefbbeea6a6a70d279d85716e11899452
yuhan007 <781567564@qq.com>
2019-02-20 10:38:00 +0800

    update

--- test-jenkins-8080/src/main/java/com/yuhan007/Test_Jenkins_8080.java
+++ test-jenkins-8080/src/main/java/com/yuhan007/Test_Jenkins_8080.java
@@ -8,8 +8,5 @@ public class Test_Jenkins_8080 {
     public static void main(String[] args) {
         SpringApplication.run(Test_Jenkins_8080.class, args);
         System.out.println("a");
-        System.out.println("b");
-        System.out.println("c");
-        System.out.println("d");
     }
 }
